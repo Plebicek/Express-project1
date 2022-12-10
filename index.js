891978596c9d5da38a0f411b121d2493be64444e
plebicek <lagt@seznam.cz>
2022-12-10 18:34:29 +0100

    handlebars init

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5500;
 const handlebars = require("express-handlebars");
 
+/* hbs */
+
 /* engine */
-app.engine("handlebars", handlebars.engine());
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layout",
+    partialsDir: "views/partials",
+  })
+);
 app.set("view engine", "handlebars");
-app.set("views", "./views");
 
 app.get("/", (req, res) => {
   console.log("HomePage");
-  res.send("<h1>homePage</h1>");
+  res.render("index", { layout: "main", style: "index.css", title: "Home" });
 });
+
+/* Static Files */
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
